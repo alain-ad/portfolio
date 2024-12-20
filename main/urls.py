f294bc675b76aa16c4ae0586ac6f1c1ba2d0d85f
alain-ad <rjalainad@gmail.com>
2024-12-20 18:41:23 +0300

    configuration urls for the application main

---
+++ main/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+urlpaterns=[
+    path('', views.home, name= 'home'),
+    path('about/', views.about, name='about'),
+    path('projects/', views.projects, name='projects'),
+    path('contact/', views.contact, name='contact'),
+]
